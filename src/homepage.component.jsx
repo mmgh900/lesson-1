54572bcb2d6d77259efc97b7dcfaeb8c1cd330d2
mmgh900 <gheysari.mm@gmail.com>
2021-03-03 20:07:46 +0330

    Make a homepage.component.jsx

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='menu-directory'>
+            <div className='menu-item'>
+                <div className='menu-item__content'>
+                    <h1 className='menu-item__title'>HATS</h1>
+                    <span className='menu-item__subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='menu-item__content'>
+                    <h1 className='menu-item__title'>JACKETS</h1>
+                    <span className='menu-item__subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='menu-item__content'>
+                    <h1 className='menu-item__title'>SNEAKERS</h1>
+                    <span className='menu-item__subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='menu-item__content'>
+                    <h1 className='menu-item__title'>WOMEN</h1>
+                    <span className='menu-item__subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='menu-item__content'>
+                    <h1 className='menu-item__title'>MEN</h1>
+                    <span className='menu-item__subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+)
+export default HomePage;
